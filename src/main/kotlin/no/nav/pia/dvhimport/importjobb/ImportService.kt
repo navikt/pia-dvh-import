35c3c5b83dd6e3ab758fdf16b8a8f2a7e3734a5f
Thomas Dufourd <thomas.dufourd@gmail.com>
2025-02-04 13:34:16 +0100

    Tilpasse ImportService til load-test i dev-gcp

--- src/main/kotlin/no/nav/pia/dvhimport/importjobb/ImportService.kt
+++ src/main/kotlin/no/nav/pia/dvhimport/importjobb/ImportService.kt
@@ -138,23 +138,30 @@ class ImportService(
         path: String,
         årstallOgKvartal: ÅrstallOgKvartal,
     ) {
+        val skalSendeTilKafka = brukÅrOgKvartalIPathTilFilene == false // TODO: DELETE ME etter load-test
         val sumAntallTapteDagsverk = AtomicReference(BigDecimal(0))
         val sumAntallMuligeDagsverk = AtomicReference(BigDecimal(0))
+        val sumAntallVirksomheter = AtomicReference(0)
         val sequence = bucketKlient.getFromHugeFileAsSequence<VirksomhetSykefraværsstatistikkDto>(
             path = path,
             fileName = tilFilNavn(StatistikkKategori.VIRKSOMHET),
         )
         sequence.prosesserIBiter(størrelse = 100) { statistikk ->
             logger.info("Sender ${statistikk.size} statistikk for virksomhet til Kafka")
-            sendTilKafka(
-                årstallOgKvartal = årstallOgKvartal,
-                statistikk,
-            )
+            if (skalSendeTilKafka) {
+                logger.info("Skal IKKE sende til kafka i load-test")
+                sendTilKafka(
+                    årstallOgKvartal = årstallOgKvartal,
+                    statistikk,
+                )
+            }
+            sumAntallVirksomheter.getAndAccumulate(statistikk.size) { x, y -> x + y }
             sumAntallMuligeDagsverk.getAndAccumulate(statistikk.sumOf { it.muligeDagsverk }) { x, y -> x + y }
             sumAntallTapteDagsverk.getAndAccumulate(statistikk.sumOf { it.tapteDagsverk }) { x, y -> x + y }
         }
         val sykefraværsprosentForKategori =
             StatistikkUtils.kalkulerSykefraværsprosent(sumAntallTapteDagsverk.get(), sumAntallMuligeDagsverk.get())
+        logger.info("Antall statistikk prosessert for kategori ${StatistikkKategori.VIRKSOMHET.name} er: '$sumAntallVirksomheter'")
         logger.info("Sykefraværsprosent -snitt- for kategori ${StatistikkKategori.VIRKSOMHET.name} er: '$sykefraværsprosentForKategori'")
     }
 
